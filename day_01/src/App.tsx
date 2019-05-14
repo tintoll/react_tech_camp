45937f964bbfd1faad0b5099982b6e000b1a7ccc
tintoll <belselios@gmail.com>
2019-05-15 08:30:38 +0900

    1일차 소스 복습

--- day_01/src/App.tsx
+++ day_01/src/App.tsx
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { MonitorCard, PlayButton } from "./components";
+import { Typography } from "antd";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "antd/dist/antd.css";
+import "./sass/main.scss";
+
+interface Application {
+  timerId: number;
+  state : {
+    success: number;
+    failure: number;
+  };
+  onStart(): void;
+  onStop(): void;
 }
 
-export default App;
+export default class App extends React.Component implements Application {
+  timerId = 0;
+  state = {
+    success: 0,
+    failure : 0
+  }
+
+  onStart = () => {
+    this.timerId = setInterval(() => {
+      // this.setState({
+      //   success: this.state.success + Math.floor(Math.random() * (100 - 1)),
+      //   failure: this.state.failure + Math.floor(Math.random() * 2 - 0)
+      // });
+      this.setState((prevState: any) => {
+        return {
+          success: prevState.success + Math.floor(Math.random() * (100 - 1)),
+          failure: prevState.failure + Math.floor(Math.random() * 2 - 0)
+        }
+      });
+    }, 200);
+  }
+  onStop = () => {
+    clearTimeout(this.timerId);
+    this.timerId = 0;
+  }
+  render() {
+    return (
+      <div>
+        <header>
+          <Typography.Title>React & TS Bolierplate</Typography.Title>
+          <Typography>Order Monitor</Typography>
+        </header>
+        <main>
+          <MonitorCard 
+              success={this.state.success}
+              failure={this.state.failure}
+            />
+          <PlayButton 
+              monitoring={false}
+              onPlay={this.onStart}
+              onPause={this.onStop}
+            />  
+        </main>
+      </div>
+    );
+  }
+}
--- day_01/src/index.tsx
+++ day_01/src/index.tsx
@@ -3,6 +3,7 @@ import { render } from "react-dom";
 import App from "./App";
 
 // 타입을 모르면 any로 사용하면 된다.
-const rootElement: HTMLElement = document.getElementById("root");
+//const rootElement: HTMLElement = document.getElementById("root");
+//render(<App />, rootElement);
 
-render(<App />, rootElement);
+render(<App />, document.getElementById("root"));
